8420d5bfc4fb0c65ae367e17a1053af6a92e9a3a
koenhagen <koenhagen@gmail.com>
2023-10-05 23:03:55 +0200

    run unit tests

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
 try {
     console.time("energy");
     const { exec } = require('child_process');
 
-    exec('sar -u 1 240 -o test.txt', (err, stdout, stderr) => {
+    exec('sar -u 1 5 -o test.txt', (err, stdout, stderr) => {
         console.log(err, stdout, stderr)
     });
 
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('what-to-test');
-    console.log(`Hello ${nameToGreet}!`);
+    const unitTest = core.getInput('what-to-test');
+    exec(unitTest, (err, stdout, stderr) => {
+        console.log(unitTest)
+        console.log(err, stdout, stderr)
+    });
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
 
     console.timeEnd("energy");
 } catch (error) {
