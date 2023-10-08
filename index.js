0578c3d1b5b3a03a84678ceff90b492356eea1a6
koenhagen <koenhagen@gmail.com>
2023-10-08 18:31:47 +0200

    issues.createComment

--- index.js
+++ index.js
@@ -2,37 +2,17 @@ const core = require('@actions/core');
 const { exec } = require('child_process');
 const os = require('os');
 const github = require('@actions/github');
-const { Octokit } = require("@octokit/rest");
 
 try {
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    const github_token = core.getInput('GITHUB_TOKEN');
 
-    const octokit = new Octokit();
-    const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
-    octokit.issues.createComment({
+    const octokit = github.getOctokit(github_token);
+    // const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+    octokit.rest.issues.createComment({
         ...github.context.repo,
         issue_number: github.context.payload.pull_request.number,
         body: 'message'
     }).then(result => console.log(`result ${result}`))
-
-    // octokit.request('POST /repos/{owner}/{repo}/pulls/{pull_number}/comments', {
-    //     owner: owner,
-    //     repo: repo,
-    //     pull_number: github.context.payload.pull_request.number,
-    //     body: 'Great stuff!',
-    //     commit_id: github.context.payload.issue.,
-    //     path: 'file1.txt',
-    //     start_line: 1,
-    //     start_side: 'RIGHT',
-    //     line: 2,
-    //     side: 'RIGHT',
-    //     headers: {
-    //         'X-GitHub-Api-Version': '2022-11-28'
-    //     }
-    // }).then(data => {
-    //     console.log(data);
-    // })
 
     const cpus = os.cpus();
     const cpu = cpus[0];
