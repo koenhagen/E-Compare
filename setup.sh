0a6d374ffd5a22c1b5c399c4fa3a7476cf2de4c5
koenhagen <koenhagen@gmail.com>
2023-12-01 13:00:44 +0100

    killall

--- setup.sh
+++ setup.sh
@@ -2,27 +2,27 @@
 
 echo "Setup python"
 
-# Create a venv, and backup old
-python3 -m venv /tmp/eco-ci/venv
-
-VENV_VALUE=${VIRTUAL_ENV:-}
-PREVIOUS_VENV=''
-
-if [[ $VENV_VALUE != '' ]]; then
-   PREVIOUS_VENV=$VENV_VALUE
-   source "$(dirname "$0")/vars.sh" add_var PREVIOUS_VENV $PREVIOUS_VENV
-fi
-
-#  Installing requirements
-# first activate our venv
-source /tmp/eco-ci/venv/bin/activate
-python3 -m pip install -r /tmp/eco-ci/spec-power-model/requirements.txt
-# now reset to old venv
-deactivate our venv
-# reactivate the old one, if it was present
-if [[ $PREVIOUS_VENV != '' ]]; then
-  source $PREVIOUS_VENV/bin/activate
-fi
+## Create a venv, and backup old
+#python3 -m venv /tmp/eco-ci/venv
+#
+#VENV_VALUE=${VIRTUAL_ENV:-}
+#PREVIOUS_VENV=''
+#
+#if [[ $VENV_VALUE != '' ]]; then
+#   PREVIOUS_VENV=$VENV_VALUE
+#   source "$(dirname "$0")/vars.sh" add_var PREVIOUS_VENV $PREVIOUS_VENV
+#fi
+#
+##  Installing requirements
+## first activate our venv
+#source /tmp/eco-ci/venv/bin/activate
+#python3 -m pip install -r /tmp/eco-ci/spec-power-model/requirements.txt
+## now reset to old venv
+#deactivate our venv
+## reactivate the old one, if it was present
+#if [[ $PREVIOUS_VENV != '' ]]; then
+#  source $PREVIOUS_VENV/bin/activate
+#fi
 
 echo "Clone spec-power-model"
 
@@ -35,6 +35,5 @@ chmod +x /tmp/demo-reporter
 
 echo "Run demo-reporter"
 
-/tmp/demo-reporter | tee -a /tmp/cpu-util-total.txt > /tmp/cpu-util.txt &
-
-cat /tmp/cpu-util-total.txt
+killall -9 -q /tmp/demo-reporter || true
+/tmp/demo-reporter > /tmp/cpu-util.txt &
