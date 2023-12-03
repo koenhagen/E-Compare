66439762caf83507c41b019407692bd614a318f2
koenhagen <koenhagen@gmail.com>
2023-12-03 15:29:47 +0100

    oldData to JSON

--- index.js
+++ index.js
@@ -163,7 +163,9 @@ async function getMeasurementsFromRepo(octokit, sha) {
             path,
             ref,
         });
+        console.log(`Old data: ${oldData}`);
         console.log(`Old data: ${oldData.data}`);
+        console.log(`Old data: ${Base64.decode(String(oldData))}`);
         console.log(`Old data: ${Base64.decode(String(oldData.data))}`);
         return JSON.parse(Base64.decode(String(oldData.data[0].content)));
         // return JSON.parse(fs.readFileSync(`./.energy/${sha}.json`, 'utf8'));
