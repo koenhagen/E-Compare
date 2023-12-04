e23af310f7e749e5af70492a08389693bc0b053d
koenhagen <koenhagen@gmail.com>
2023-12-04 20:49:36 +0100

    setup.js

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clone --depth 1 --single-branch --branch main https://github.com/green-coding-berlin/spec-power-model /tmp/spec-power-model
-python3 -m pip install -r /tmp/spec-power-model/requirements.txt
-
-gcc /tmp/spec-power-model/demo-reporter/cpu-utilization.c -o /tmp/demo-reporter
-chmod +x /tmp/demo-reporter
