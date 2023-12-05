4e94270135db3a5a6589de5e26740fe651a5ae7f
koenhagen <koenhagen@gmail.com>
2023-12-05 17:40:41 +0100

    our venv

--- dist/index.js
+++ dist/index.js
@@ -18,7 +18,7 @@ const run = function run(modelData) {
         execSync(`cat /tmp/cpu-util.txt | python3.10 /tmp/spec-power-model/xgb.py --silent --tdp ${modelData['TDP']} --cpu-threads ${modelData['CPU_THREADS']} --cpu-cores ${modelData['CPU_CORES']} --cpu-make ${modelData['CPU_MAKE']} --release-year ${modelData['RELEASE_YEAR']} --ram ${modelData['RAM']} --cpu-freq ${modelData['CPU_FREQ']} --cpu-chips ${modelData['CPU_CHIPS']} --vhost-ratio ${modelData['VHOST_RATIO']} > /tmp/energy.txt`);
 
         // Deactivate virtual environment
-        execSync('deactivate');
+        execSync('deactivate our venv');
     } catch (error) {
 
         process.exit(1);
