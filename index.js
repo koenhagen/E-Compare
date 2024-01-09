d4adcd5888d34ed90c75d905ea05a13fe14565d8
koenhagen <koenhagen@gmail.com>
2024-01-09 15:38:49 +0100

    log

--- index.js
+++ index.js
@@ -117,6 +117,7 @@ async function run_push() {
 
         const octokit = retrieveOctokit();
         const new_data = readEnergyData();
+        console.log(`Measured data: ${new_data}`);
         await commitReport(octokit, new_data);
 
         const pull_request = await getPullRequest(octokit, github.context.sha);
